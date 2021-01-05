76a482cd5e7c0874b780c237d2ae82f1180c99fa
Nikhil <doosarinikhil@gamil.com>
2021-01-05 11:44:00 +0530

    mongo removed

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,6 @@ import * as path from "path";
 import bodyparser from 'body-parser';
 import { Server, createServer } from "http";
 import * as sslserver from "https";
-import { connect, connection } from "mongoose";
 import { attachRoutes } from './helpers/decorators';
 import * as socket from "./socket/socket";
 import { Session as session } from "./services/session"
@@ -61,20 +60,20 @@ class App {
 
 	}
 
-	private _connectToDatabase() {
-		const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DB } = process.env;
-		const mongoDBUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${MONGO_DB}`;
+	// private _connectToDatabase() {
+	// 	const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DB } = process.env;
+	// 	const mongoDBUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${MONGO_DB}`;
 
-		connect(mongoDBUrl, { useNewUrlParser: true, useFindAndModify: false });
+	// 	connect(mongoDBUrl, { useNewUrlParser: true, useFindAndModify: false });
 
-		connection.once("open", () => {
-			console.info("Connected to mongo");
-		});
+	// 	connection.once("open", () => {
+	// 		console.info("Connected to mongo");
+	// 	});
 
-		connection.on('error', (err) => {
-			console.error('Unable to connect to Mongo via Mongoose', err);
-		});
-	}
+	// 	connection.on('error', (err) => {
+	// 		console.error('Unable to connect to Mongo via Mongoose', err);
+	// 	});
+	// }
 	private _startSession() {
 		session.connect();
 	}
