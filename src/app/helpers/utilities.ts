1d3bffb0c22295aa41dadc5b52db74d8b6802805
Nikhil <doosarinikhil@gamil.com>
2021-06-26 14:34:17 +0530

    caqll type changes

--- src/app/helpers/utilities.ts
+++ src/app/helpers/utilities.ts
@@ -61,7 +61,7 @@ function constructAlertData(payload: any,type: string){
                 "isMute": false,
                 "isRecorded": false,
                 "callRecordedFile":null,
-                "callType": payload.callType,
+                "callType": payload.participants[0].callType,
                 "isConference": false,
                 "callOwner": payload.from.id,
                 "callTimestamp": seconds_since_epoch(new Date()),
